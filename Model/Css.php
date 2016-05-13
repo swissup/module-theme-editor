75b75bf03baed06e8b57d6fce00810b1e63d232d
Roman Chumak <chumakrom@gmail.com>
2016-05-13 11:50:19 +0300

    Css generating based on admin config implemented

---
+++ Model/Css.php
@@ -0,0 +1,312 @@
+<?php
+namespace Swissup\ThemeEditor\Model;
+
+use Magento\Store\Model\ScopeInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+class Css
+{
+    /** Themes */
+    const ABSOLUTE_THEME  = 'absolute_theme';
+    const ARGENTO_ESSENCE = 'argento_essence';
+
+    /** Modes */
+    const MODE_CREATE_AND_SAVE = 'create_save';
+    const MODE_UPDATE          = 'update';
+
+    /**
+     * @var \Magento\Framework\Message\ManagerInterface
+     */
+    protected $messageManager;
+    /**
+     * @var \Magento\MediaStorage\Model\File\Storage\File
+     */
+    protected $mediaStorage = null;
+    /**
+     * Config data loader
+     *
+     * @var \Magento\Config\Model\Config\Loader
+     */
+    protected $configLoader;
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $scopeConfig;
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $storeManager;
+
+    /**
+     * @param \Magento\Framework\Message\ManagerInterface $messageManager
+     * @param \Magento\MediaStorage\Model\File\Storage\FileFactory $mediaStorageFactory
+     * @param \Magento\Config\Model\Config\Loader $configLoader
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     */
+    public function __construct(
+        \Magento\Framework\Message\ManagerInterface $messageManager,
+        \Magento\MediaStorage\Model\File\Storage\FileFactory $mediaStorageFactory,
+        \Magento\Config\Model\Config\Loader $configLoader,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Store\Model\StoreManagerInterface $storeManager
+    ) {
+        $this->messageManager = $messageManager;
+        $this->mediaStorage = $mediaStorageFactory->create();
+        $this->configLoader = $configLoader;
+        $this->scopeConfig = $scopeConfig;
+        $this->storeManager = $storeManager;
+    }
+
+    /**
+     * Get themes list
+     *
+     * @return array
+     */
+    public function getThemesList()
+    {
+        return [
+            self::ABSOLUTE_THEME,
+            self::ARGENTO_ESSENCE
+        ];
+    }
+
+    public function getStorage()
+    {
+        return $this->mediaStorage;
+    }
+
+    /**
+     * @param string $theme
+     * @param string $storeId
+     * @param string $websiteId
+     * @param string $mode
+     * @return void
+     */
+    public function generateAndSave($theme, $storeId, $websiteId, $mode)
+    {
+        list($storeCode, $websiteCode) = $this->getCodesFromIds($storeId, $websiteId);
+        $filePath = $this->getFilePath($theme, $storeCode, $websiteCode);
+        if (self::MODE_UPDATE === $mode) {
+            if (!file_exists($this->getStorage()->getMediaBaseDirectory() . '/' . $filePath)) {
+                return;
+            }
+        }
+        $config = $this->getThemeConfig($theme, $storeId, $websiteId);
+        $css    = $this->convertConfigToCss($theme, $config);
+        try {
+            $this->getStorage()->saveFile([
+                'content'  => $css,
+                'filename' => $filePath
+            ], true);
+        } catch (\Exception $e) {
+            $this->messageManager->addError($e->getMessage());
+        }
+    }
+
+    /**
+     * @param string $theme
+     * @param string $storeId
+     * @param string $websiteId
+     * @return void
+     */
+    public function removeFile($theme, $storeId, $websiteId)
+    {
+        list($storeCode, $websiteCode) = $this->getCodesFromIds($storeId, $websiteId);
+        $filePath = $this->getFilePath($theme, $storeCode, $websiteCode);
+        @unlink($this->getStorage()->getMediaBaseDirectory() . '/' . $filePath);
+    }
+
+    /**
+     * Retrieve css filepath, relative to media folder
+     *
+     * @param string $theme
+     * @param string $storeCode
+     * @param string $websiteCode
+     * @return string
+     */
+    public function getFilePath($theme, $storeCode, $websiteCode)
+    {
+        $suffix = '_backend.css';
+        if ($storeCode) {
+            $prefix = implode('_', [
+                $websiteCode,
+                $storeCode
+            ]);
+        } elseif ($websiteCode) {
+            $prefix = $websiteCode;
+        } else {
+            $prefix = 'admin';
+        }
+        return str_replace('_', '/', $theme) . '/' . 'css' . '/' . $prefix . $suffix;
+    }
+
+    /**
+     * @param string $theme
+     * @param string $storeId
+     * @param string $websiteId
+     * @return array
+     */
+    public function getThemeConfig($theme, $storeId, $websiteId)
+    {
+        if ($storeId) {
+            $scope     = ScopeInterface::SCOPE_STORES;
+            $scopeCode = $storeId;
+        } elseif ($websiteId) {
+            $scope     = ScopeInterface::SCOPE_WEBSITES;
+            $scopeCode = $websiteId;
+        } else {
+            $scope     = ScopeConfigInterface::SCOPE_TYPE_DEFAULT;
+            $scopeCode = null;
+        }
+        $node = $this->configLoader->getConfigByPath(
+            $theme,
+            ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
+            null
+        );
+        if (!$node) {
+            return [];
+        }
+
+        $config = [];
+        foreach ($node as $group => $values) {
+            $value = $this->scopeConfig->getValue($group, $scope, $scopeCode);
+            $groupId = explode('/', $group)[1];
+            $valueId = $this->camel2dashed(explode('/', $values['path'])[2]);
+            $config[$groupId][$valueId] = $value;
+        }
+
+        return $config;
+    }
+    /**
+     * @param String $theme
+     * @param array $config
+     * <pre>
+     *  background
+     *      body_background-color => #fff
+     *      ...
+     *  font
+     *      body_font-family      => Helvetica,Arial,sans-serif
+     *      page-header_color     => #000
+     *      page-header_font-size => 12px
+     *      ...
+     *  style
+     *      css => inline css
+     *  css_selector
+     *      body => body
+     *      page-header => h1
+     * </pre>
+     */
+    public function convertConfigToCss($theme, $config)
+    {
+        $groupedCss   = [];
+        $groupsToSkip = ['css_selector', 'head'];
+        $propsToSkip  = ['heading', 'head_link', 'sticky_header'];
+        foreach ($config as $groupName => $groupValues) {
+            if (in_array($groupName, $groupsToSkip)) {
+                continue;
+            }
+            foreach ($groupValues as $name => $value) {
+                $value = (string)$value;
+                list($key, $prop) = explode('_', $name);
+                if (in_array($prop, $propsToSkip)) {
+                    continue;
+                }
+                if ($method = $this->_getExtractorMethodName($prop)) {
+                    $value = $this->$method($value);
+                }
+                if (false === $value || strlen($value) === 0) {
+                    continue; // feature to keep default theme styles from theme.css
+                }
+                $groupedCss[$key][] = "{$prop}:{$value};";
+            }
+        }
+        $css = '';
+        foreach ($groupedCss as $key => $cssArray) {
+            if (empty($config['css_selector'])
+                || !is_array($config['css_selector'])
+                || empty($config['css_selector'][$key])) {
+                $selector = $this->scopeConfig
+                    ->getValue($theme . '/css_selector/' .$key);
+                if (empty($selector)) {
+                    continue;
+                }
+            } else {
+                $selector = $config['css_selector'][$key];
+            }
+            $styles   = implode('', $cssArray);
+            $css .= "{$selector}{{$styles}}\n";
+        }
+        if (!empty($config['head']['css'])) {
+            $css .= $config['head']['css'];
+        }
+        return $css;
+    }
+    /**
+     * @param string $property
+     * @return string|false
+     */
+    protected function _getExtractorMethodName($property)
+    {
+        $property = str_replace('-', ' ', $property);
+        $property = ucwords($property);
+        $property = str_replace(' ', '', $property);
+        $method = '_extract' . $property;
+        if (method_exists($this, $method)) {
+            return $method;
+        }
+        return false;
+    }
+    protected function _extractBackgroundImage($value)
+    {
+        // fix to prevent activating of 'Use default' checkbox, when image is deleted
+        if (empty($value) || 'none' === $value) {
+            $value = 'none';
+        } else {
+            $value = 'url(../images/' . $value . ')';
+        }
+        return $value;
+    }
+    protected function _extractBackgroundColor($value)
+    {
+        if (empty($value)) {
+            $value = 'transparent';
+        }
+        return $value;
+    }
+    protected function _extractBackgroundPosition($value)
+    {
+        return str_replace(',', ' ', $value);
+    }
+    /**
+     * Convert capital letters to hyphens
+     * Example:
+     * in:  body_backgroundColor
+     * out: body_background-color
+     * @param  String camel case string
+     * @return String string with hyphens
+     */
+    protected function camel2dashed($str) {
+        return strtolower(preg_replace('/([a-zA-Z])(?=[A-Z])/', '$1-', $str));
+    }
+
+    /**
+     * Get website and store codes from ids
+     * @param  int $storeId
+     * @param  int $websiteId
+     * @return Array
+     */
+    private function getCodesFromIds($storeId, $websiteId)
+    {
+        $storeCode = null;
+        $websiteCode = null;
+        if ($storeId) {
+            $storeCode = $this->storeManager->getStore($storeId)->getCode();
+            $websiteCode = $this->storeManager->getStore($storeId)->getWebsite()->getCode();
+        } else if ($websiteId) {
+            $websiteCode = $this->storeManager->getWebsite($websiteId)->getCode();
+        }
+
+        return [$storeCode, $websiteCode];
+    }
+}
---
+++ Observer/Backend/ThemeEditorConfigSave.php
@@ -0,0 +1,105 @@
+<?php
+namespace Swissup\ThemeEditor\Observer\Backend;
+
+use Swissup\ThemeEditor\Model\Css as CssModel;
+
+class ThemeEditorConfigSave implements \Magento\Framework\Event\ObserverInterface
+{
+    /**
+     * @var \Swissup\ThemeEditor\Model\CssFactory
+     */
+    private $cssModelFactory;
+    /**
+     * @var \Magento\Store\Model\ResourceModel\Store\CollectionFactory
+     */
+    protected $storesFactory;
+    /**
+     * @var \Magento\Framework\App\ResourceConnection
+     */
+    protected $coreResource;
+
+    /**
+     * @param \Swissup\ThemeEditor\Model\CssFactory $cssModelFactory
+     * @param \Magento\Store\Model\ResourceModel\Store\CollectionFactory $storesFactory
+     * @param \Magento\Framework\App\ResourceConnection $coreResource
+     */
+    public function __construct(
+        \Swissup\ThemeEditor\Model\CssFactory $cssModelFactory,
+        \Magento\Store\Model\ResourceModel\Store\CollectionFactory $storesFactory,
+        \Magento\Framework\App\ResourceConnection $coreResource
+    ) {
+        $this->cssModelFactory = $cssModelFactory;
+        $this->storesFactory = $storesFactory;
+        $this->coreResource = $coreResource;
+    }
+
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $request = $observer->getControllerAction()->getRequest();
+        $section = $request->getParam('section');
+        $css = $this->cssModelFactory->create();
+        $themes = $css->getThemesList();
+
+        if (!in_array($section, $themes)) {
+            return;
+        }
+
+        $storeId = $request->getParam('store');
+        $websiteId = $request->getParam('website');
+
+        $inherited = true;
+        foreach ($request->getParam('groups') as $groupName => $groupValue) {
+            foreach ($groupValue['fields'] as $fieldName => $fieldValue) {
+                if (empty($fieldValue['inherit'])) {
+                    $inherited = false;
+                    break 2;
+                }
+            }
+        }
+        // if all options are inherited - delete file
+        if ($inherited) {
+            $css->removeFile($section, $storeId, $websiteId);
+        } else {
+            $css->generateAndSave($section, $storeId, $websiteId, CssModel::MODE_CREATE_AND_SAVE);
+        }
+        if (!$storeId) {
+            $descending = $this->getDescendingWebsiteAndStoreCodes($websiteId);
+            foreach ($descending as $_websiteCode => $_storeCodes) {
+                $css->generateAndSave($section, null, $_websiteCode, CssModel::MODE_UPDATE);
+                foreach ($_storeCodes as $_storeCode) {
+                    $css->generateAndSave($section, $_storeCode, $_websiteCode, CssModel::MODE_UPDATE);
+                }
+            }
+        }
+    }
+
+    /**
+     * Retrieve pairs of store and website ids that are the childs
+     * of given websiteId.
+     * If websiteId is null, all stores and websites will be returned.
+     *
+     * Used to update all descending stores that already has css file,
+     * to fix all inherited rules from currently saved store
+     *
+     * @param int $websiteId
+     * @return array
+     */
+    protected function getDescendingWebsiteAndStoreCodes($websiteId)
+    {
+        $collection = $this->storesFactory->create()
+            ->setWithoutDefaultFilter()
+            ->join(
+                ['website' => $this->coreResource->getTableName('store_website')],
+                'website.website_id = main_table.website_id',
+                ['website_code' => 'code']
+            );
+        if ($websiteId) {
+            $collection->addFieldToFilter('website.website_id', $websiteId);
+        }
+        $result = [];
+        foreach ($collection as $store) {
+            $result[$store->getWebsiteId()][] = $store->getId();
+        }
+        return $result;
+    }
+}
