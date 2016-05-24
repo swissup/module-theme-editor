ab1383923b994829e68819f4f1e7b59a104bd69d
Roman Chumak <chumakrom@gmail.com>
2016-05-24 10:52:43 +0300

    Color picker added in system config

---
+++ Block/System/Config/Form/Field/Color.php
@@ -0,0 +1,28 @@
+<?php
+namespace Swissup\ThemeEditor\Block\System\Config\Form\Field;
+
+class Color extends \Magento\Config\Block\System\Config\Form\Field {
+
+    protected function _getElementHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element) {
+        $html = $element->getElementHtml();
+        $value = $element->getData('value');
+
+        $html .= '<script type="text/javascript">
+            require(["jquery"], function ($) {
+                $(document).ready(function () {
+                    var $el = $("#' . $element->getHtmlId() . '");
+                    $el.css("backgroundColor", "'. $value .'");
+
+                    // Attach the color picker
+                    $el.ColorPicker({
+                        color: "'. $value .'",
+                        onChange: function (hsb, hex, rgb) {
+                            $el.css("backgroundColor", "#" + hex).val("#" + hex);
+                        }
+                    });
+                });
+            });
+            </script>';
+        return $html;
+    }
+}
