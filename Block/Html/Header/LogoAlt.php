c36527a7f4e6c35ceb2d62d78ed815c8373c3234
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2019-04-24 10:57:42 +0300

    LogoAlt refactoring. Allows to change it for custom luxury themes.

---
+++ Block/Html/Header/LogoAlt.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Swissup\ThemeEditor\Block\Html\Header;
+
+use Magento\Framework\UrlInterface;
+use Magento\Store\Model\ScopeInterface;
+
+class LogoAlt extends \Magento\Theme\Block\Html\Header\Logo
+{
+    /**
+     * @var string
+     */
+    protected $_template = 'Swissup_ThemeEditor::header/logo-alt.phtml';
+
+    /**
+     * @return string
+     */
+    protected function _getLogoUrl()
+    {
+        $logo = $this->getVar('logo_alt');
+        $folder = '';
+
+        if (is_array($logo)) {
+            if (!isset($logo['config'])) {
+                return '';
+            }
+
+            if (isset($logo['depends']) && is_array($logo['depends'])) {
+                $value = $this->getConfigValue($logo['depends']['config']);
+                if ($value != $logo['depends']['value']) {
+                    return '';
+                }
+            }
+
+            $folder = explode('/', $logo['config']);
+            $folder = str_replace('_', '/', $folder[0]) . '/images/';
+            $logo = $this->getConfigValue($logo['config']);
+        }
+
+        if (!$logo) {
+            return '';
+        }
+
+        $path = $folder . $logo;
+        if ($this->_isFile($path)) {
+            return $this->_urlBuilder
+                ->getBaseUrl(['_type' => UrlInterface::URL_TYPE_MEDIA])
+                . $path;
+        }
+
+        return '';
+    }
+
+    /**
+     * @param  string $path
+     * @param  string $scope
+     * @return mixed
+     */
+    private function getConfigValue($path, $scope = ScopeInterface::SCOPE_STORE)
+    {
+        return $this->_scopeConfig->getValue($path, $scope);
+    }
+}
