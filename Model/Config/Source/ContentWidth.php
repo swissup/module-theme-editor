45758235bc4f7b2714a04a85267edfb6cff3c673
Vitaliy Dmytruk <vitaliy@mage2cloud.com>
2020-03-25 17:52:49 +0200

    Consistent naming of drop-down option when use value from theme.

--- Model/Config/Source/ContentWidth.php
+++ Model/Config/Source/ContentWidth.php
@@ -12,7 +12,7 @@ class ContentWidth implements \Magento\Framework\Data\OptionSourceInterface
     public function toOptionArray()
     {
         return [
-            ['value' => '', 'label' => __('Don\'t change (specified by theme)')],
+            ['value' => '', 'label' => __('Specified by theme')],
             ['value' => 'full_width', 'label' => __('Full width')],
             ['value' => 'limited_width', 'label' => __('Limited width')],
         ];
