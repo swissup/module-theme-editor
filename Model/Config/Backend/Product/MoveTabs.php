90657e0e0b711d99143db08f1f4258d2174ebdfe
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2025-04-28 12:14:59 +0300

    Magento 2.4.8 fix: Fixed broken layout when tabs moved to content

--- Model/Config/Backend/Product/MoveTabs.php
+++ Model/Config/Backend/Product/MoveTabs.php
@@ -2,6 +2,8 @@
 
 namespace Swissup\ThemeEditor\Model\Config\Backend\Product;
 
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\App\ProductMetadataInterface;
 use Swissup\ThemeEditor\Model\Config\Backend\PageLayout;
 
 class MoveTabs extends PageLayout
@@ -16,8 +18,11 @@ class MoveTabs extends PageLayout
      */
     protected function _construct()
     {
+        // see https://github.com/magento/magento2/pull/39391
+        $after = $this->compareMagentoVersion('2.4.7', '<') ? 'product.info.media' : 'product.info.main';
+
         $this->mapping = [
-            'content' => '<move element="product.info.tabs" destination="content" after="product.info.media" />',
+            'content' => '<move element="product.info.tabs" destination="content" after="' . $after . '" />',
             'product.info.main' => '<move element="product.info.tabs" destination="product.info.main" after="-" />'
         ];
 
@@ -32,5 +37,12 @@ class MoveTabs extends PageLayout
         $value = $this->getData('value');
 
         return $this->mapping[$value] ?? '';
+    }
+
+    private function compareMagentoVersion($version, $operator)
+    {
+        $metadata = ObjectManager::getInstance()->get(ProductMetadataInterface::class);
+
+        return version_compare($metadata->getVersion(), $version, $operator);
     }
 }
